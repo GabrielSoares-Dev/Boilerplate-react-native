92689f22eb43bab65efa69e18110ba186f30df4b
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2025-02-25 22:12:46 -0300

    feat: configure env

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { TEST } from '@env'
 
 export function App() {
+  console.log('AAAAAAAAAAA', TEST)
   return (
     <View>
       <Text>Test</Text>
