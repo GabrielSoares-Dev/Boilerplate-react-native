f348975971d76249cfc50c34ba8ebafe86f8397f
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2025-02-25 21:57:17 -0300

    feat:configure eslint and build folder structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,30 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    '@rocketseat/eslint-config/react',
+  ],
+  ignorePatterns: ['.eslintrc.js'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/display-name': 'off',
+    'prefer-regex-literals': 'off',
+    'no-useless-escape': 'off',
+    'no-labels': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'react-hooks/rules-of-hooks': 'off',
+  },
 };
